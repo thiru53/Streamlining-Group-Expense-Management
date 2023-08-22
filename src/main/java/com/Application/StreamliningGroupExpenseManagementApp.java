17f78e785795225d705d663a2952e0d16fdea85b
Tiru <thiru53@gmail.com>
2023-08-22 20:14:16 +0530

    task-2

---
+++ src/main/java/com/Application/StreamliningGroupExpenseManagementApp.java
@@ -0,0 +1,11 @@
+package com.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamliningGroupExpenseManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamliningGroupExpenseManagementApp.class, args);
+    }
+}
