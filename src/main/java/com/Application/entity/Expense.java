c69e654c2f41878acb3950b49eb1fa6217af4c68
Tiru <thiru53@gmail.com>
2023-08-27 22:40:16 +0530

    task-5

--- src/main/java/com/Application/entity/Expense.java
+++ src/main/java/com/Application/entity/Expense.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 import jakarta.persistence.*;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "expenses")
 public class Expense {
